f3c5ee0c85306c30a4a95b221079f1e4a584ff3a
Tomas Duclos <duclosiw@gmail.com>
2023-04-05 17:53:22 -0400

    initializing project with nextjs 13 appDir

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: path.join(__dirname, 'tsconfig.json')
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import 'tailwindcss/tailwind.css'
+
+export default function Layout ({
+  children
+}: any) {
+  return (
+    <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body className="bg-stone-950 text-white">
+      <main>
+        {children}
+      </main>
+    </body>
+    </html>
+  )
+}
